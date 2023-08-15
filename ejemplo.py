e8b585df6e906c29bc246d1b2d2045ec623b9a77
309 <309@academicos.cesde.edu.co>
2023-08-15 08:06:46 -0500

    feat:creamos variables primitvivas y lista de datos

---
+++ ejemplo.py
@@ -0,0 +1,11 @@
+#cometarios linea
+nombreUsuario ="Juan david chaverra"
+edadUsuario =19
+estaturaUsuario=1.81
+esHinchaDelverde=True 
+
+print(f"Su nombre es: {nombreUsuario} y tiene {edadUsuario} años.")
+
+comidasFavoritas =["Frisoles","sushicharron","mazamorra","carne molida"]
+print (comidasFavoritas)
+print (comidasFavoritas [2])
